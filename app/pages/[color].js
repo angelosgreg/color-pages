e32f60ed865ea7a1a8e2cc670d0adbaaafada1ce
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-11-21 15:26:36 +0100

    css

--- app/pages/[color].js
+++ app/pages/[color].js
@@ -15,3 +15,14 @@ export async function getStaticPaths() {
   // ]
   return { paths, fallback: false }
 }
+export async function getStaticProps({ params }) {
+  // find the info for just one color
+  const color = colors.find(color => color.name === params.color)
+  // return it in the necessary format.
+  return { props: { color } }
+}
+export default function Color({ color }) {
+  return <div className='color-page' style={{ backgroundColor: color.hex }}>
+    <h1>{color.name}</h1>
+  </div>
+}
